048956c08d015c91e732c00836c3da8d4127422f
katty <a20160555@pucp.edu.pe>
2020-05-04 19:20:21 -0500

    employee entity repository falta de controller

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,12 +1,29 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 //COMPLETAR
+@Controller
+@RequestMapping(value = "/employee")
 public class EmployeeController {
 
     //COMPLETAR
+    @Autowired
+    EmployeesRepository employeeRepository;
 
+    @GetMapping("/list")
     public String listaEmployee(   ){
         //COMPLETAR
+        List<Employees> listaEmp = employeeRepository.findAll();
+        model.addAttribute("lista", listaEmp);
+        return "employee/lista";
     }
 
     public String nuevoEmployeeForm( ) {
