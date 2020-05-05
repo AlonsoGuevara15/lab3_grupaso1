3b6c7a34e91adba3ce3ed13c8028041b932eee70
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-05-04 20:08:17 -0500

    search

--- src/main/java/com/example/laboratorio3/controller/SearchController.java
+++ src/main/java/com/example/laboratorio3/controller/SearchController.java
@@ -1,8 +1,14 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.dto.mayorSalario;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 //COMPLETAR
 
@@ -11,13 +17,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SearchController {
 
     //COMPLETAR
+    @Autowired
+    EmployeesRepository employeesRepository;
 
-    @GetMapping(value = {"","/"})
-    public String indice(){
+    @GetMapping(value = {"", "/"})
+    public String indice() {
         return "Search/indice";
     }
 
+    @GetMapping("/mayorsalario")
+    public String mayorsalario(Model m) {
+        List<mayorSalario> mayorSalarios = employeesRepository.obtenerEmpleadosSalario();
+        m.addAttribute("lista",mayorSalarios);
+        return "Search/empleadoMayorSalario";
+    }
 
+    @GetMapping("/salarioDepart")
+    public String salarioporDepa() {
+        return "Search/salarioDepartamento";
+    }
+
+    @GetMapping("/depaPaisCiudad")
+    public String depaPaisCiudad() {
+        return "Search/departamentoPaisCiudad";
+    }
+
+    @GetMapping("/gerenteExperiencia")
+    public String gerenteExperiencia() {
+        return "Search/gerentesExperiencia";
+    }
 //COMPLETAR
 
 
